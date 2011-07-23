9b8d3ea4cd0c36c04df20b4cde911d1a97ac07de
Aleksey V Zapparov <ixti@member.fsf.org>
2011-07-23 22:22:00 +0200

    Reverted all crap related to baseurl

--- lib/toto.rb
+++ lib/toto.rb
@@ -196,10 +196,6 @@ module Toto
       def method_missing m, *args, &blk
         @context.respond_to?(m) ? @context.send(m, *args, &blk) : super
       end
-
-      def baseurl
-        'http://' + (@config[:url].sub("http://", '') + '/' + @config[:prefix]).squeeze('/')
-      end
     end
   end
 
